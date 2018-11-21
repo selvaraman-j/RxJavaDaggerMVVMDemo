f87169b32d7a77a997ef591f6e943b3b4689f31e
sraman <sraman@brilliantbasics.com>
2018-11-21 23:12:07 +0000

    Added unit test for web service response

--- app/src/test/java/demo/selva/com/rxjavadaggermvvmdemo/viewmodel/CurrencyConversionViewModelTest.kt
+++ app/src/test/java/demo/selva/com/rxjavadaggermvvmdemo/viewmodel/CurrencyConversionViewModelTest.kt
@@ -1,7 +1,9 @@
 package demo.selva.com.rxjavadaggermvvmdemo.viewmodel
 
 import demo.selva.com.rxjavadaggermvvmdemo.Repository.CurrencyAPIService
+import demo.selva.com.rxjavadaggermvvmdemo.model.Currency
 import demo.selva.com.rxjavadaggermvvmdemo.viewmodel.helper.DateUtil
+import io.reactivex.Single
 import junit.framework.TestCase.assertEquals
 import org.junit.Before
 import org.junit.Test
@@ -13,6 +15,7 @@ class CurrencyConversionViewModelTest {
 
     companion object {
         private const val INR_GBP = 95.0
+        private const val INR_GBP_FAILURE = 0.0
         private const val NO_OF_GBP = 2.0
         private const val TOTAL = NO_OF_GBP * INR_GBP
         private const val LAST_UPDATE = "17-Nov-2018 11:04:16"
@@ -22,6 +25,8 @@ class CurrencyConversionViewModelTest {
     lateinit var currencyAPIService: CurrencyAPIService
     @Mock
     lateinit var dateUtil: DateUtil
+    @Mock
+    lateinit var currency: Currency
 
     private lateinit var currencyConversionViewModel: CurrencyConversionViewModel
 
@@ -29,6 +34,20 @@ class CurrencyConversionViewModelTest {
     fun setUp() {
         MockitoAnnotations.initMocks(this)
         currencyConversionViewModel = CurrencyConversionViewModel(currencyAPIService, dateUtil)
+    }
+
+    @Test
+    fun getCurrentCurrencyRateResponseTest() {
+        `when`(currency.gbpinr).then { INR_GBP }
+        `when`(currencyAPIService.getCurrentCurrencyRate()).then { Single.just(currency) }
+        assertEquals(INR_GBP, currencyConversionViewModel.getCurrentCurrencyRate().blockingGet())
+    }
+
+    @Test
+    fun getCurrentCurrencyRateFailureResponseTest() {
+        `when`(currency.gbpinr).then { null }
+        `when`(currencyAPIService.getCurrentCurrencyRate()).then { Single.just(currency) }
+        assertEquals(INR_GBP_FAILURE, currencyConversionViewModel.getCurrentCurrencyRate().blockingGet())
     }
 
     @Test
